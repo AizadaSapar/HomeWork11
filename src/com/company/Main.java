d65f558b11bf59369e99a1620c80e823f2b394a7
Aizada <aizada.alybaeva.kk@gmail.com>
2022-05-05 12:56:56 +0600

    Homework 11

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+       /* Создайте класс Triangle, у класса должны быть поля a, b, c.
+                В классе создайте метод "area" который при вызове выводит на консоль площадь треугольника*/
+        Triangle jaim = new Triangle();
+        Scanner scanner = new Scanner(System.in);
+        jaim.a = scanner.nextInt();
+        jaim.b = scanner.nextInt();
+        jaim.c = scanner.nextInt();
+        jaim.area(jaim.a, jaim.b, jaim.c);
+    }
+
+}
---
+++ src/com/company/Triangle.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Triangle {
+    int a;
+    int b;
+    int c;
+
+    void area(int a, int b, int c) {
+        System.out.println("Площадь треугольника: "+(a+b+c)/2);
+
+    }
+}
